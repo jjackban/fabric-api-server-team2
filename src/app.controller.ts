5b4f933e2e8ff39edeb293e6600d498038e06362
jcentury <tprl0328@naver.com>
2024-06-24 13:39:02 +0900

    team2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,8 +6,38 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index')
-  root() {
+  getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/init')
+  async init(
+    @Query('user') user: string,
+    @Query('userval') userval: string,
+  ): Promise<string> {
+    return this.appService.init(user, userval);
+  }
+
+  @Get('/invoke')
+  async invoke(
+    @Query('sender') sender: string,
+    @Query('reciever') reciever: string,
+    @Query('value') value: string,
+  ): Promise<string> {
+    return this.appService.invoke(sender, reciever, value);
+  }
+
+  @Get('/query')
+  async query(
+    @Query('name') name: string,
+  ): Promise<string> {
+    return this.appService.query(name);
+  }
+
+  @Get('/delete')
+  async delete(
+    @Query('name') name: string,
+  ): Promise<string> {
+    return this.appService.delete(name);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { send } from './util/connectFabric';
 
 @Injectable()
 export class AppService {
-  getHello() {
-    return {message: 'Hello World!'};
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  async init(user:string, userval:string) {
+    const args = [user, userval];
+    return await send(false, 'init', args);
+  }
+
+  async invoke(sender:string, reciever:string, value:string) {
+    const args = [sender, reciever, value];
+    return await send(false, 'invoke', args);
+  }
+
+  async query(name:string) {
+    const args = [name];
+    return await send(true, 'query', args);
+  }
+
+  async delete(name:string) {
+    const args = [name];
+    return await send(false, 'delete', args);
   }
 }
